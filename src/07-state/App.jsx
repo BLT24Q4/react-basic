562eac35187de859d8f3506eb95c4afea8e96754
Sean Nam <blt24q4@gmail.com>
2024-12-18 12:23:02 +0900

    Implement Create Component

--- src/07-state/App.jsx
+++ src/07-state/App.jsx
@@ -57,6 +57,8 @@ function App() {
   //   const mode = "WELCOME";
   //   const mode = "READ";
   const [mode, setMode] = useState("WELCOME");
+  //    현재 선택된 게시물의 아이디 저장 상태 변수
+  const [id, setId] = useState(null);
   /*
   const [state이름, state변경함수] = useState(초기state값);
   */
@@ -74,7 +76,22 @@ function App() {
     content = <Article title="Welcome" body="Hello, WEB"></Article>;
   } else if (mode === "READ") {
     //  게시물 페이지 구성
-    content = <Article title="WEB" body="Hello, READ" />;
+
+    //  id state와 topic의 id가 동일한 객체 찾기
+    let title = null,
+      body = null;
+
+    //  검색
+    for (let i = 0; i < topics.length; i++) {
+      let topic = topics[i];
+
+      if (topic.id === Number(id)) {
+        title = topic.title;
+        body = topic.body;
+        break;
+      }
+    }
+    content = <Article title={title} body={body} />;
   }
 
   return (
@@ -88,9 +105,11 @@ function App() {
       ></Header>
       <Nav
         topics={topics}
-        onChangeMode={(id) => {
+        onChangeMode={(_id) => {
           //   alert(id);
           setMode("READ");
+          //    선택된 게시물 _id -> id 상태변수로 설정
+          setId(_id);
         }}
       ></Nav>
       {/* mode에 따라 다른 페이지 구성 */}
